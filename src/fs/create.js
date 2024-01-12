720a88ab68eccc4340ab037e7d208e01889ffaab
Mikita <anykey2solo@gmail.com>
2024-01-13 01:17:40 +0300

    feat: fs/create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,32 @@
-const create = async () => {
-    // Write your code here 
+/** @format */
+const path = require("path");
+const { writeFile } = require("node:fs/promises");
+const { open, close } = require("node:fs");
+const FILE_NAME = "fresh.txt";
+const FOLDER_NAME = "files";
+
+const CONTENT_TEXT = "I am fresh and young";
+const ERROR_TEXT = "FS operation failed";
+const ERROR_CODE = "EEXIST";
+
+const create = () => {
+  const pathName = path.join(__dirname, FOLDER_NAME, FILE_NAME);
+
+  open(pathName, "wx", async (err, fd) => {
+    if (err && err.code === ERROR_CODE) {
+      throw new Error(ERROR_TEXT);
+    } else {
+      try {
+        await writeFile(pathName, CONTENT_TEXT, (err) => {
+          if (err) throw err;
+        });
+      } finally {
+        close(fd, (err) => {
+          if (err) throw err;
+        });
+      }
+    }
+  });
 };
 
-await create();
+create();
