d2aede1d6e4abfa776d4b292d9646d0b06c9364a
Mikita <anykey2solo@gmail.com>
2024-01-13 15:33:25 +0300

    feat: cli:args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,21 @@
+/** @format */
+
+const { argv } = require("node:process");
+
+const flag = "--";
+
 const parseArgs = () => {
-    // Write your code here 
+  const argsArray = argv.slice(2);
+  let result = "";
+
+  argsArray.forEach((el, index, array) => {
+    const flagIndex = el.indexOf(flag);
+
+    if (flagIndex === 0) {
+      result += `${el.slice(2)} is ${array[index + 1]}, `;
+    }
+  });
+  console.log(result);
 };
 
 parseArgs();
