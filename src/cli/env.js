86d0e8dea988032a8ba67711f2069f93b5c76e2b
Mikita <anykey2solo@gmail.com>
2024-01-13 15:07:18 +0300

    feat: cli:env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,17 @@
+/** @format */
+const { env } = require("node:process");
+
+const envPrefix = "RSS_";
+
 const parseEnv = () => {
-    // Write your code here 
+  let result = "";
+  for (let e in env) {
+    const isRSSenv = e.indexOf(envPrefix) === 0;
+    if (isRSSenv) {
+      result += `${e}=${env[e]}; `;
+    }
+  }
+  console.log(result);
 };
 
 parseEnv();
