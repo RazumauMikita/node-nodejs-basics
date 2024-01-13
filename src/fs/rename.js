b56d2e280b8aafc95f57e62060d31d6f29fa5e40
Mikita <anykey2solo@gmail.com>
2024-01-13 13:48:29 +0300

    feat: fs:rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,21 @@
+/** @format */
+
+const fs = require("fs");
+const path = require("path");
+
+const WRONG_FILE_NAME = "wrongFilename.txt";
+const PROPER_FILE_NAME = "properFilename.md";
+const FOLDER_NAME = "files";
+const ERROR_TEXT = "FS operation failed";
+
 const rename = async () => {
-    // Write your code here 
+  fs.rename(
+    path.join(__dirname, FOLDER_NAME, WRONG_FILE_NAME),
+    path.join(__dirname, FOLDER_NAME, PROPER_FILE_NAME),
+    (err) => {
+      if (err) throw new Error(ERROR_TEXT);
+    }
+  );
 };
 
-await rename();
+rename();
