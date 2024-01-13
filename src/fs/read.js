efd3d6490fde7cbd84e13f21da746f6a5001967e
Mikita <anykey2solo@gmail.com>
2024-01-13 14:30:55 +0300

    feat: fs:read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,25 @@
+/** @format */
+
+const fs = require("fs");
+const path = require("path");
+
+const FOLDER_NAME = "files";
+const ERROR_TEXT = "FS operation failed";
+
 const read = async () => {
-    // Write your code here 
+  fs.readdir(path.join(__dirname, FOLDER_NAME), (err, files) => {
+    if (err) throw new Error(ERROR_TEXT);
+    files.forEach((file) => {
+      fs.readFile(
+        path.join(__dirname, FOLDER_NAME, file),
+        "utf-8",
+        (err, data) => {
+          if (err) throw new Error(ERROR_TEXT);
+          console.log(data);
+        }
+      );
+    });
+  });
 };
 
-await read();
+read();
