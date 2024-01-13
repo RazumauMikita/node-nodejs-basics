a54d1bea22b410a055b53f3311863f1239a3114b
Mikita <anykey2solo@gmail.com>
2024-01-13 14:17:02 +0300

    feat: fs:list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,16 @@
+/** @format */
+
+const fs = require("fs");
+const path = require("path");
+
+const FOLDER_NAME = "files";
+const ERROR_TEXT = "FS operation failed";
+
 const list = async () => {
-    // Write your code here 
+  fs.readdir(path.join(__dirname, FOLDER_NAME), (err, files) => {
+    if (err) throw new Error(ERROR_TEXT);
+    console.log(files);
+  });
 };
 
-await list();
+list();
