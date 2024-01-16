7466358b7cd4f36ffa1d61d442177637e8432556
Mikita <anykey2solo@gmail.com>
2024-01-16 18:48:20 +0300

    feat: hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
-const calculateHash = async () => {
-    // Write your code here 
-};
+/** @format */
 
-await calculateHash();
+const fs = require("fs");
+const path = require("path");
+const crypto = require("crypto");
+const FILE_NAME = "fileToCalculateHashFor.txt";
+const FOLDER_NAME = "files";
+
+const calculateHash = () => {
+  const readableStream = fs.createReadStream(
+    path.join(__dirname, FOLDER_NAME, FILE_NAME)
+  );
+  readableStream.on("data", (chunk) => {
+    const hashSum = crypto.createHash("sha256");
+    hashSum.update(chunk);
+    const hex = hashSum.digest("hex");
+    console.log(hex);
+  });
+};
+calculateHash();
