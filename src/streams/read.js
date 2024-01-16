a2cb1252a201b17d9e15a5be965465a96e0fd045
Mikita <anykey2solo@gmail.com>
2024-01-16 19:44:37 +0300

    feat: stream: readeble

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
-const read = async () => {
-    // Write your code here 
+/** @format */
+
+const fs = require("fs");
+const path = require("path");
+const { pipeline } = require("stream");
+
+const FILE_NAME = "fileToRead.txt";
+const FOLDER_NAME = "files";
+
+const out = process.stdout;
+const input = fs.createReadStream(path.join(__dirname, FOLDER_NAME, FILE_NAME));
+
+const read = () => {
+  pipeline(input, out, (err) => err);
 };
 
-await read();
+read();
