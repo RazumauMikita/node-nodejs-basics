545a9c9d4d150700023c2058676ac950caae1175
Mikita <anykey2solo@gmail.com>
2024-01-17 19:32:01 +0300

    feat: zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,23 @@
+/** @format */
+
+const fs = require("fs");
+const zlib = require("zlib");
+const { pipeline } = require("stream");
+const path = require("path");
+
+const FILE_NAME = "fileToCompress.txt";
+const COMPRESSED_FILE_NAME = "archive.gz";
+const FOLDER_NAME = "files";
+
 const compress = async () => {
-    // Write your code here 
+  const input = fs.createReadStream(
+    path.join(__dirname, FOLDER_NAME, FILE_NAME)
+  );
+  const out = fs.createWriteStream(
+    path.join(__dirname, FOLDER_NAME, COMPRESSED_FILE_NAME)
+  );
+  const gzip = zlib.createGzip();
+  pipeline(input, gzip, out, (err) => err);
 };
 
-await compress();
+compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,23 @@
+/** @format */
+const fs = require("fs");
+const zlib = require("zlib");
+const { pipeline } = require("stream");
+const path = require("path");
+
+const FILE_NAME = "archive.gz";
+const DECOMPRESSED_FILE_NAME = "fileToCompress.txt";
+const FOLDER_NAME = "files";
+
 const decompress = async () => {
-    // Write your code here 
+  const input = fs.createReadStream(
+    path.join(__dirname, FOLDER_NAME, FILE_NAME)
+  );
+  const out = fs.createWriteStream(
+    path.join(__dirname, FOLDER_NAME, DECOMPRESSED_FILE_NAME)
+  );
+  const unzip = zlib.createUnzip();
+
+  pipeline(input, unzip, out, (err) => err);
 };
 
-await decompress();
+decompress();
