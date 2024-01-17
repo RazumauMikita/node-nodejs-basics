9050928cad077901877e1b1115fedc0057b57ff1
Mikita <anykey2solo@gmail.com>
2024-01-17 18:43:13 +0300

    feat: stream: transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,22 @@
+/** @format */
+
+const fs = require("fs");
+const { pipeline, Transform } = require("stream");
+
+const input = process.stdin;
+const out = process.stdout;
+
 const transform = async () => {
-    // Write your code here 
+  const trans = new Transform({
+    transform(chunk, en, callback) {
+      const chunkStringified = chunk.toString().trim();
+      const reversedChunk = chunkStringified.split("").reverse().join("");
+      callback(null, reversedChunk + "\n");
+    },
+  });
+  pipeline(input, trans, out, (err) => {
+    throw err;
+  });
 };
 
-await transform();
+transform();
